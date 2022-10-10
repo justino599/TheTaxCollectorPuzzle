c7ae127ab106979fa72f742e75e0ea3551c7c2ed
Justin Schoenit <77038122+justino599@users.noreply.github.com>
2022-10-10 13:04:58 -0700

    Initial Commit

---
+++ src/Game.java
@@ -0,0 +1,102 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Game implements Comparable<Game>, Cloneable {
+    int profit = 0;
+    ArrayList<Integer> moves = new ArrayList<>();
+    boolean[] available; // Used to more efficiently determine if a cheque is available. We could use cheques.contains(i), but that is much less efficient than available[i]
+    ArrayList<Integer>[] factors;
+    int numOfCheques;
+
+    public Game(int numOfCheques) {
+        this.numOfCheques = numOfCheques;
+        available = new boolean[numOfCheques + 1];
+        for (int i = 1; i <= numOfCheques; i++)
+            available[i] = true;
+
+        // Create a reference table that holds all the factors of each number
+        factors = new ArrayList[numOfCheques + 1];
+        for (int i = 1; i <= numOfCheques; i++) {
+            ArrayList<Integer> factorsList = new ArrayList<>();
+            for (int j = 1; j <= i/2; j++)
+                if (i%j==0)
+                    factorsList.add(j);
+            factors[i] = factorsList;
+        }
+    }
+
+    public void take(int cheque) {
+        if (available[cheque]) {
+            // Make sure that the tax collector will have something to take
+            if (factors[cheque].size() == 0)
+                throw new IllegalArgumentException(String.format("Taking cheque %d from %s does not leave anything for the tax collector", cheque, Arrays.toString(available)));
+
+            ArrayList<Integer> chequesTaken = new ArrayList<>();
+
+            // Take your cheque
+            profit += cheque;
+            available[cheque] = false;
+            moves.add(cheque);
+            chequesTaken.add(cheque);
+
+            // Let the tax collector take his cheques
+            for (int i : factors[cheque]) {
+                available[i] = false;
+                chequesTaken.add(i);
+            }
+
+            // Remove taken cheques from factors table
+            for (int removedCheque : chequesTaken)
+                for (int i = 1; i <= numOfCheques; i++)
+                    factors[i].remove(Integer.valueOf(removedCheque));
+
+        } else {
+            throw new IllegalArgumentException(String.format("Cheque %d not available in %s", cheque, Arrays.toString(available)));
+        }
+    }
+
+    public ArrayList<ArrayList<Integer>> possibleMoves() {
+        ArrayList<ArrayList<Integer>> moves = new ArrayList<>();
+
+        for (int i = 1; i <= numOfCheques; i++) {
+            if (available[i] && factors[i].size() > 0) {
+                ArrayList<Integer> move = new ArrayList<>();
+                move.add(i);
+                move.addAll(factors[i]);
+                moves.add(move);
+            }
+        }
+
+        return moves;
+    }
+
+    @Override
+    public int compareTo(Game o) {
+        return this.profit - o.profit;
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        Game clone = (Game) super.clone();
+        clone.moves = (ArrayList<Integer>) this.moves.clone();
+        clone.available = this.available.clone();
+        ArrayList<Integer>[] clonedFactors = new ArrayList[numOfCheques + 1];
+        for (int i = 1; i <= numOfCheques; i++) {
+            clonedFactors[i] = (ArrayList<Integer>) factors[i].clone();
+        }
+        clone.factors = clonedFactors;
+
+
+        return clone;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "profit=" + profit +
+                ", moves=" + moves +
+                ", available=" + Arrays.toString(available) +
+                ", factors=" + Arrays.toString(factors) +
+                ", numOfCheques=" + numOfCheques +
+                '}';
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        for (int i = 2; i <= 36; i+=2) {
+            long start = System.currentTimeMillis();
+
+            Game game = new Game(i);
+            Game bestGame = bruteForceBestGame(game);
+
+            System.out.println("Best game with " + i + " cheques has a profit of $" + bestGame.profit + ", using the following moves: " + bestGame.moves + " (" + (System.currentTimeMillis() - start) + "ms)");
+        }
+    }
+
+    public static Game bruteForceBestGame(Game game) throws CloneNotSupportedException {
+        ArrayList<ArrayList<Integer>> moves = game.possibleMoves();
+
+        ArrayList<Game> games = new ArrayList<>();
+
+        for (ArrayList<Integer> move : moves) {
+            Game clonedGame = (Game) game.clone();
+            clonedGame.take(move.get(0));
+            games.add(bruteForceBestGame(clonedGame));
+        }
+
+        if (games.size() > 0) {
+            Game max = games.get(0);
+            for (int i = 1; i < games.size(); i++)
+                if (games.get(i).compareTo(max) > 0)
+                    max = games.get(i);
+
+            return max;
+        }
+        else {
+            return game;
+        }
+    }
+}
