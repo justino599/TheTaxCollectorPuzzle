244d6fa0727d7fc9d50639d70af5a91b21445d4e
Justin Schoenit <77038122+justino599@users.noreply.github.com>
2022-10-12 00:29:01 -0700

    Added a new algorithm and added runtime options

--- README.md
+++ README.md
@@ -2,18 +2,18 @@
 
 Here I will paste all solutions my code has generated. These solutions are not necessarily the only solutions, but they are tied to be the best solution. Solutions calculated so far:
 
-Best game with 2 has a profit of $2, using the following moves: [2] (1ms)\
-Best game with 4 has a profit of $7, using the following moves: [3, 4] (0ms)\
-Best game with 6 has a profit of $15, using the following moves: [5, 4, 6] (0ms)\
-Best game with 8 has a profit of $21, using the following moves: [7, 6, 8] (1ms)\
-Best game with 10 has a profit of $40, using the following moves: [7, 9, 6, 8, 10] (1ms)\
-Best game with 12 has a profit of $50, using the following moves: [11, 8, 9, 10, 12] (3ms)\
-Best game with 14 has a profit of $66, using the following moves: [13, 8, 9, 10, 12, 14] (11ms)\
-Best game with 16 has a profit of $89, using the following moves: [13, 9, 15, 10, 12, 14, 16] (45ms)\
-Best game with 18 has a profit of $111, using the following moves: [17, 9, 15, 10, 14, 18, 12, 16] (44ms)\
-Best game with 20 has a profit of $124, using the following moves: [19, 10, 14, 15, 20, 12, 16, 18] (106ms)\
-Best game with 22 has a profit of $166, using the following moves: [19, 9, 15, 21, 14, 18, 12, 16, 20, 22] (797ms)\
-Best game with 24 has a profit of $182, using the following moves: [23, 9, 15, 21, 14, 16, 18, 20, 22, 24] (1749ms)\
-Best game with 26 has a profit of $224, using the following moves: [23, 25, 15, 21, 14, 16, 18, 20, 22, 24, 26] (23260ms)\
-Best game with 28 has a profit of $279, using the following moves: [23, 25, 15, 21, 14, 22, 26, 27, 18, 28, 16, 20, 24] (165978ms)\
-Best game with 30 has a profit of $301, using the following moves: [29, 22, 25, 15, 21, 26, 27, 18, 30, 20, 16, 24, 28] (648872ms)
+2 cheques: Profit of $2, using moves: [2] (1ms)\
+4 cheques: Profit of $7, using moves: [3, 4] (0ms)\
+6 cheques: Profit of $15, using moves: [5, 4, 6] (1ms)\
+8 cheques: Profit of $21, using moves: [7, 6, 8] (0ms)\
+10 cheques: Profit of $40, using moves: [7, 9, 6, 8, 10] (2ms)\
+12 cheques: Profit of $50, using moves: [11, 8, 9, 10, 12] (6ms)\
+14 cheques: Profit of $66, using moves: [13, 8, 9, 10, 12, 14] (10ms)\
+16 cheques: Profit of $89, using moves: [13, 9, 15, 10, 12, 14, 16] (41ms)\
+18 cheques: Profit of $111, using moves: [17, 9, 15, 10, 14, 18, 12, 16] (80ms)\
+20 cheques: Profit of $124, using moves: [19, 10, 14, 15, 20, 12, 16, 18] (106ms)\
+22 cheques: Profit of $166, using moves: [19, 9, 15, 21, 14, 18, 12, 16, 20, 22] (843ms)\
+24 cheques: Profit of $182, using moves: [23, 9, 15, 21, 14, 16, 18, 20, 22, 24] (1754ms)\
+26 cheques: Profit of $224, using moves: [23, 25, 15, 21, 14, 16, 18, 20, 22, 24, 26] (22025ms)\
+28 cheques: Profit of $279, using moves: [23, 25, 15, 21, 14, 22, 26, 27, 18, 28, 16, 20, 24] (161365ms)\
+30 cheques: Profit of $301, using moves: [29, 22, 25, 15, 21, 26, 27, 18, 30, 20, 16, 24, 28] (648872ms)
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,94 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws CloneNotSupportedException {
-        for (int i = 2; i <= 36; i+=2) {
-            long start = System.currentTimeMillis();
+    public static void main(String[] args) throws CloneNotSupportedException, IOException {
+        Scanner in = new Scanner(System.in);
 
-            Game game = new Game(i);
-            Game bestGame = bruteForceBestGame(game);
+        System.out.println("Algorithms:\n(1) Brute Force\n(2) Algo 1");
+        System.out.print("Choose an algorithm: ");
+        int algorithm = in.nextInt();
 
-            System.out.println(i + " cheques: Profit of $" + bestGame.playerProfit + ", using moves: " + bestGame.moves + " (" + (System.currentTimeMillis() - start) + "ms)");
+        System.out.print("Verbose output (y/n)? ");
+        boolean verbose = in.next().toLowerCase(Locale.ROOT).charAt(0) == 'y';
+
+        System.out.print("Check for up to N cheques? ");
+        in.nextLine();
+        String input = in.nextLine().strip();
+        Scanner in2 = new Scanner(input);
+        int numCheques = in2.nextInt();
+        int step;
+        if (in2.hasNextInt())
+            step = in2.nextInt();
+        else
+            step = 2;
+        in2.close();
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss");
+        LocalDateTime now = LocalDateTime.now();
+
+        File f = new File("logs\\" + dtf.format(now) + ".log");
+        f.createNewFile();
+
+        try (PrintStream logger = new PrintStream(f)) {
+            for (int i = Math.max(2,step); i <= numCheques; i += step) {
+                long start = System.currentTimeMillis();
+
+                Game game = new Game(i);
+                Game bestGame = switch (algorithm) {
+                    case 1 -> bruteForceBestGame(game);
+                    case 2 -> algo1(game);
+                    default -> game;
+                };
+
+                String output = verbose ? i + " cheques: Profit of $" + bestGame.playerProfit + ", using moves: " + bestGame.moves + " (" + (System.currentTimeMillis() - start) + "ms)" : String.format("%-4s| $%s", i, bestGame.playerProfit);
+                logger.println(output);
+                System.out.println(output);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
+    }
+
+    public static Game algo1(Game game) {
+        // Take the largest prime first
+        int largestPrime = 2;
+        for (int i = game.numOfCheques; i > 2; i--) {
+            if (prime(i)) {
+                largestPrime = i;
+                break;
+            }
+        }
+        game.take(largestPrime);
+
+        // Then take cheques with only 1 remaining factor. Choose cheque with the largest difference between user's cheque and tax collector's cheque(s)
+        ArrayList<ArrayList<Integer>> moves;
+        while ((moves = game.possibleMoves()).size() > 0) {
+            // Sort moves from best to worst according to the above described algorithm
+            moves.sort((o1, o2) -> {
+                if (o1.size() != o2.size()) {
+                    // Sort moves that have the tax collector taking less cheques to the top
+                    return o1.size() - o2.size();
+                } else {
+                    // Sort moves that have a greater difference between user and tax collector payouts to the top
+                    int diff1 = o1.get(0) - sumOfFactors(o1);
+                    int diff2 = o2.get(0) - sumOfFactors(o2);
+                    return diff2 - diff1;
+                }
+            });
+
+            // Make the best move
+            game.take(moves.get(0).get(0));
+        }
+
+        return game;
     }
 
     public static Game bruteForceBestGame(Game game) throws CloneNotSupportedException {
@@ -34,5 +113,24 @@ public class Main {
         else {
             return game;
         }
+    }
+
+    public static ArrayList<Integer> factors(int i) {
+        ArrayList<Integer> factorsList = new ArrayList<>();
+        for (int j = 1; j <= i/2; j++)
+            if (i%j==0)
+                factorsList.add(j);
+        return factorsList;
+    }
+
+    public static boolean prime(int i) {
+        return factors(i).size() < 2;
+    }
+
+    public static int sumOfFactors(ArrayList<Integer> move) {
+        int sum = 0;
+        for (int i = 1; i < move.size(); i++)
+            sum += move.get(i);
+        return sum;
     }
 }
